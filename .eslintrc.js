da29b11bd0d21b9ad66bcad9ae34c8143433a66e
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-10 16:33:19 -0500

    🎉 [BEGIN] New project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'prettier/prettier': 0,
+  },
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
